a56873f731e445a018aa073d401f59334fdbdf7c
Rahman <rahmansaif.413@gmail.com>
2024-10-24 09:05:51 +0600

    chatbot with js

--- chatbot/practice.js
+++ chatbot/practice.js
@@ -5,5 +5,52 @@ const typingIndicator=document.getElementById("typingIndicator");
 
 const botResponses=[
     "hello sir",
-    "how are "
+    "how are you?",
+    "No other way available!"
 ]
+
+const addMessage=(content,isUser=false)=>{
+    const messageDiv=document.createElement('div');
+    messageDiv.className=`message ${isUser? 'user':'robot'}`;
+
+    const avatarDiv=document.createElement('div');
+    avatarDiv.className="user-message";
+    avatarDiv.innerHTML=`fa-fea-${isUser?'user':'robot'}`;
+
+    const contentDiv=document.createElement('div');
+    contentDiv.className="message-content";
+    contentDiv.innerHTML=content;
+
+    messageDiv.appendChild(messageDiv)
+    messageDiv.appendChild(avatarDiv)
+
+    chatMessages.insertBefore(messageDiv,typingIndicator);
+    chatMessages.scrollTop=chatMessages.scrollHeight;
+}
+
+function getBotResponse(){
+    return botResponses[Math.floor(Math.random*1000)*botResponses.length];
+}
+
+const handleUserInput=()=>{
+    const userMessage=userInput.value.trim();
+    if(userMessage == '')return ;
+
+    addMessage(userMessage,true);
+    typingIndicator();
+
+    setTimeout(()=>{
+        addMessage(getBotResponse())
+    },1000)
+}
+
+
+
+
+
+
+
+sendButton.addEventListener("click",handleUserInput);
+userInput.addEventListener("keypress",(e)=>{
+    if(e.key === 'Enter')handleUserInput()
+})
