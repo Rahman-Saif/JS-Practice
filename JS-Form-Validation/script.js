618acc416f1b826fd5e0b0faddd0c4179c76d51f
Rahman <rahmansaif.413@gmail.com>
2024-11-01 07:44:11 +0600

    Validation Form Added

--- JS-Form-Validation/script.js
+++ JS-Form-Validation/script.js
@@ -1,35 +1,34 @@
-let id=(id)=>document.getElementById(id);
+let id = (id) => document.getElementById(id);
 
-let classes=(classes)=>document.getElementsByClassName(classes);
+let classes = (classes) => document.getElementsByClassName(classes);
 
-let username=id("username"),
-    form=id("form"),
-    email=id("email"),
-    password=id("password"),
-    errorMsg=classes("error"),
-    successIcon=classes("success-icon"),
-    failureIcon=classes("failure-icon");
+let username = id("username"),
+    form = id("form"),
+    email = id("email"),
+    password = id("password"),
+    errorMsg = classes("error"),
+    successIcon = classes("success-icon"),
+    failureIcon = classes("failure-icon");
 
-    form.addEventListener("click",(e)=>{
-      e.preventDefault();
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-      engine(username, 0 ,"username cant be blanked");
-      engine(email,1,"email can't be blanked");
-      engine(password,2,"password can't be blanked");
-    });
+    engine(username, 0, "Username can't be blank");
+    engine(email, 1, "Email can't be blank");
+    engine(password, 2, "Password can't be blank");
+});
 
-    let engine=(nam,i,msg) => {
-      if(nam.value.trim()===""){
-        errorMsg[i].innerHTML=msg;
-        nam.style.border="2px solid red";
+let engine = (nam, i, msg) => {
+    if (nam.value.trim() === "") {
+        errorMsg[i].innerHTML = msg;
+        nam.style.border = "2px solid red";
 
-        successIcon[i].style.opacity="0";
-        failureIcon[i].style.opacity="1";
-      }
-      else {
-        errorMsg[i].innerHTML="";
-        nam.style.border="2px solid green";
-        successIcon[i].style.opacity="1";
-        failureIcon[i].style.opacity="0";
-      }
-    };
+        successIcon[i].style.opacity = "0";
+        failureIcon[i].style.opacity = "1";
+    } else {
+        errorMsg[i].innerHTML = "";
+        nam.style.border = "2px solid green";
+        successIcon[i].style.opacity = "1";
+        failureIcon[i].style.opacity = "0";
+    }
+};
